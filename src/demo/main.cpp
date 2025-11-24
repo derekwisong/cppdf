742f3c7587a382f6ccbdc364a03a7c41937d102b
Derek <derekwisong@gmail.com>
2025-11-24 13:06:39 -0500

    gen

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -3,10 +3,22 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <random>
 
 int main() {
-    df::DataFrame df;
-    df.add("c1", df::Series<int>({1, 2, 3, 4, 5}));
-    std::cout << "df len: " << df.length() << "\n";
-    std::cout << df.column<int>("c1") << "\n";
+    constexpr std::size_t N{1'000'000'000};
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<double> dist(0.0, 1.0);
+
+    std::vector<double> data(N);
+    data.reserve(N);
+
+    std::generate_n(data.begin(), N, [&]() { return dist(gen); });
+    
+    return 0;
+    
+    // std::cout << "df len: " << df.length() << "\n";
+    // std::cout << df.column<int>("c1") << "\n";
 }
