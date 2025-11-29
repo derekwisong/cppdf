cfe25755e151d71f7471ee13da22a3fbe0029788
Derek Wisong <derekwisong@gmail.com>
2025-11-29 18:23:53 -0500

    replace array with constexpr vars

--- src/benchmarks/main.cpp
+++ src/benchmarks/main.cpp
@@ -1,49 +1,48 @@
 #include "df.h"
+#include "rng.h"
 
-#include <array>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <execution>
 #include <benchmark/benchmark.h>
-#include "rng.h"
 
 
 namespace {
     using namespace df;
     constexpr auto NUM_CALCS{1'000'000};
 
-	template<typename T>
+	template<typename T=double>
     Series<T> generate_random_series(std::size_t N) {
         return Series<T>(bench::generate_random_mt<T>(N));
 	}
 
     void calc1_loop(benchmark::State& state) {
-        auto c1 = generate_random_series<double>(NUM_CALCS);
-        constexpr std::array<double, 3> c2({0.98, 1.0, 0.9});
+        auto c1 = generate_random_series(NUM_CALCS);
+        constexpr double a = 0.98, b = 1.0, c = 0.9;
         for (auto _ : state) {
             std::transform(
                 std::execution::par_unseq,
                 c1.begin(),
                 c1.end(),
                 c1.begin(),
-                [&](double x) { return c2[2] + std::exp(c2[0] + c2[1] * x); }
+                [&](double x) { return c + std::exp(a + b * x); }
             );
         }
     }
     BENCHMARK(calc1_loop);
 
     void calc1_series(benchmark::State& state) {
-        auto c1 = generate_random_series<double>(NUM_CALCS);
-        constexpr std::array<double, 3> c2({0.98, 1.0, 0.9});
+        auto c1 = generate_random_series(NUM_CALCS);
+        constexpr double a = 0.98, b = 1.0, c = 0.9;
         for (auto _ : state) {
-            c1.mul(c2[1]).add(c2[0]).exp().add(c2[2]);
+            c1.mul(b).add(a).exp().add(c);
         }
     }
     BENCHMARK(calc1_series);
 
     void add_scalar(benchmark::State& state) {
-        auto c1 = generate_random_series<double>(NUM_CALCS);
+        auto c1 = generate_random_series(NUM_CALCS);
         const auto val = c1[0];
         for (auto _ : state) {
             c1.add(val);
@@ -52,16 +51,15 @@ namespace {
     BENCHMARK(add_scalar);
 
     void add_series(benchmark::State& state) {
-        auto c1 = generate_random_series<double>(NUM_CALCS);
-        auto c2 = generate_random_series<double>(NUM_CALCS);
+        auto c1 = generate_random_series(NUM_CALCS);
         for (auto _ : state) {
-            c1.add(c2);
+            c1.add(c1);
         }
     }
     BENCHMARK(add_series);
 
     void mul_scalar(benchmark::State& state) {
-        auto c1 = generate_random_series<double>(NUM_CALCS);
+        auto c1 = generate_random_series(NUM_CALCS);
         const auto val = c1[0];
         for (auto _ : state) {
             c1.mul(val);
@@ -70,16 +68,15 @@ namespace {
     BENCHMARK(mul_scalar);
 
     void mul_series(benchmark::State& state) {
-        auto c1 = generate_random_series<double>(NUM_CALCS);
-        auto c2 = generate_random_series<double>(NUM_CALCS);
+        auto c1 = generate_random_series(NUM_CALCS);
         for (auto _ : state) {
-            c1.mul(c2);
+            c1.mul(c1);
         }
     }
     BENCHMARK(mul_series);
 
     void sqrt_series(benchmark::State& state) {
-        auto c1 = generate_random_series<double>(NUM_CALCS);
+        auto c1 = generate_random_series(NUM_CALCS);
         for (auto _ : state) {
             c1.sqrt();
         }
@@ -87,7 +84,7 @@ namespace {
     BENCHMARK(sqrt_series);
 
     void exp_series(benchmark::State& state) {
-        auto c1 = generate_random_series<double>(NUM_CALCS);
+        auto c1 = generate_random_series(NUM_CALCS);
         for (auto _ : state) {
             c1.exp();
         }
